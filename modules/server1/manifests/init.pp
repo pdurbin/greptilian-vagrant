199b2c57034332c4d09ffcd9448bfd7efa322461
Philip Durbin <philipdurbin@gmail.com>
2012-10-27 19:21:51 -0400

    specify "modules" in file path

--- modules/server1/manifests/init.pp
+++ modules/server1/manifests/init.pp
@@ -39,49 +39,49 @@ class server1 {
   }
 
   file { '/etc/gitweb.conf':
-    source => 'puppet:///server1/etc/gitweb.conf',
+    source => 'puppet:///modules/server1/etc/gitweb.conf',
     owner  => 'root',
     group  => 'root',
     mode   => '0444',
   }
 
   #file { '/etc/yum.repos.d/greptilian.repo':
-  #  source => 'puppet:///server1/etc/yum.repos.d/greptilian.repo',
+  #  source => 'puppet:///modules/server1/etc/yum.repos.d/greptilian.repo',
   #  owner  => 'root',
   #  group  => 'root',
   #  mode   => '0444',
   #}
 
   file { '/etc/httpd/conf.d/0greptilian.conf':
-    source => 'puppet:///server1/etc/httpd/conf.d/0greptilian.conf',
+    source => 'puppet:///modules/server1/etc/httpd/conf.d/0greptilian.conf',
     owner  => 'root',
     group  => 'root',
     mode   => '0444',
   }
 
   file { '/etc/httpd/conf.d/welcome.conf':
-    source => 'puppet:///server1/etc/httpd/conf.d/welcome.conf',
+    source => 'puppet:///modules/server1/etc/httpd/conf.d/welcome.conf',
     owner  => 'root',
     group  => 'root',
     mode   => '0444',
   }
 
   file { '/etc/httpd/conf.d/yum.greptilian.com.conf':
-    source => 'puppet:///server1/etc/httpd/conf.d/yum.greptilian.com.conf',
+    source => 'puppet:///modules/server1/etc/httpd/conf.d/yum.greptilian.com.conf',
     owner  => 'root',
     group  => 'root',
     mode   => '0444',
   }
 
   file { '/etc/httpd/conf.d/git.greptilian.com.conf':
-    source => 'puppet:///server1/etc/httpd/conf.d/git.greptilian.com.conf',
+    source => 'puppet:///modules/server1/etc/httpd/conf.d/git.greptilian.com.conf',
     owner  => 'root',
     group  => 'root',
     mode   => '0444',
   }
 
   file { '/usr/sbin/server1-puppet-apply.sh':
-    source => 'puppet:///server1/usr/sbin/server1-puppet-apply.sh',
+    source => 'puppet:///modules/server1/usr/sbin/server1-puppet-apply.sh',
     owner  => 'root',
     group  => 'root',
     mode   => '0555',
@@ -89,7 +89,7 @@ class server1 {
 
   # http://docs.puppetlabs.com/references/stable/metaparameter.html#notify
   file { '/etc/sysconfig/iptables':
-    source => 'puppet:///server1/etc/sysconfig/iptables',
+    source => 'puppet:///modules/server1/etc/sysconfig/iptables',
     notify => Service['iptables'],
     owner  => 'root',
     group  => 'root',
