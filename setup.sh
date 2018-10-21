33b8130cdc900e944e90965d5a8ff73c2e962f21
Philip Durbin <philipdurbin@gmail.com>
2018-10-20 21:04:16 -0400

    mysql working on boot, philbot connect on boot

--- setup.sh
+++ setup.sh
@@ -18,7 +18,9 @@ mysqladmin -u root password root
 mysql -u root -proot -e 'create database moritz5;'
 mysql -u root -proot -e "grant all privileges on moritz5.* to 'moritz'@'localhost' identified by 'foo';"
 mysql -u root -proot -e 'flush privileges;'
-LATEST_MYSQL_BACKUP=mysql_backup_20181019-230501.sql
+# just the irclog database or else mysql won't restart after boot
+#LATEST_MYSQL_BACKUP=mysql_backup_20181019-230501.sql
+LATEST_MYSQL_BACKUP=mysql_backup_irclog_20181020-202239.sql
 wget http://data.greptilian.com/mysql/${LATEST_MYSQL_BACKUP}.bz2
 bunzip2 ${LATEST_MYSQL_BACKUP}.bz2
 cat $LATEST_MYSQL_BACKUP | mysql -u root -proot moritz5
@@ -29,15 +31,21 @@ git clone https://github.com/pdurbin/philbot.git
 cd philbot
 cp -a bot.conf bot.conf.orig
 cp /greptilian/files/var/www/irclog.greptilian.com/bot.conf .
+# configure bot for testing
+# cp /greptilian/bot.conf /var/www/irclog.greptilian.com/philbot/bot.conf
 # ./ilbot2.pl # quick test
 mkdir /usr/local/lib/site_perl
 mkdir /usr/local/lib/site_perl/IrcLog
 cp /var/www/irclog.greptilian.com/philbot/lib/IrcLog.pm /usr/local/lib/site_perl/IrcLog.pm
 cp /var/www/irclog.greptilian.com/philbot/lib/IrcLog/WWW.pm /usr/local/lib/site_perl/IrcLog/WWW.pm
 # use /var/www/irclog.greptilian.com/philbot/database.conf in /usr/local/lib/site_perl/IrcLog.pm
+cp /var/www/irclog.greptilian.com/philbot/database.conf /var/www/irclog.greptilian.com/philbot/cgi/database.conf
+# /var/www/irclog.greptilian.com/cgi vs. /var/www/irclog.greptilian.com/philbot/cgi
 a2enmod cgi
 a2enmod rewrite
+# cp -a /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf.`date +'%s'`
+# cp /greptilian/irclog.greptilian.com.conf /etc/apache2/sites-available/000-default.conf
 service apache2 restart
-./index.pl
 
+#./index.pl
 touch $HAS_BEEN_SETUP
